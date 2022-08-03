048fd67c70bd68d5ac4e702e46875929b2be8bb2
programist <zhanybaevttt@gmail.com>
2022-08-03 18:15:56 +0300

    commit

--- src/com/copmpany/Main.java
+++ src/com/copmpany/Main.java
@@ -1,5 +1,13 @@
+package com.copmpany;
+
 public class Main {
     public static void main(String[] args) {
-        
+    try (Car car =new Car()){
+        car.drive();
+    } catch (Exception e) {
+        e.getMessage();
+
+    }
+
     }
 }
